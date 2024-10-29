5ea0f8965675c2384a234e8cc9599113454df3ce
Mariana Zakseski <140620547+mariszakseski@users.noreply.github.com>
2024-10-29 08:38:48 -0300

    29 out

---
+++ perguntas.js
@@ -0,0 +1,60 @@
+criaCartao(
+    'Biologia',
+    'O que são órgãos análogos?',
+    'São órgãos de espécies diferentes, com mesma função e origem embrionária diferentes'
+)
+criaCartao(
+    'História',
+    'Onde ocorreu a Guerra de Canudos?',
+    'sertão nordestino'
+)
+criaCartao(
+    'Biologia',
+    'O que a genética estuda?',
+    'A hereditariedade, a estrutura e função dos genes e a variação dos seres vivos'
+)
+criaCartao(
+    'Robótica',
+    'O que é buzzer passivo?',
+    'É o componente eletrônico que atua como um mini auto falante'
+)
+criaCartao(
+    'Empreendedorismo',
+    'O que é Freemium?',
+    'São empresas que oferecem um serviço free gratuito e cobram pelo serviço premium'
+)
+criaCartao(
+    'Biotecnologia',
+    'Como as briófitas se reproduzem?',
+    'Se reproduzem através de esporos levados pela água'
+)
+criaCartao(
+    'Geografia',
+    'O que é desigualdade socioeconômica?',
+    'É a diferença entre as diferentes classes sociais'
+)
+criaCartao(
+    'Sociologia',
+    'Defina o capitalismo',
+    'Se caracteriza pela propriedade privada dos meios de produção, com divisão social e técnica do trabalho'
+)
+criaCartao(
+    'Sociologia',
+    'Explique o conceito de mais-valia',
+    'Representa a desigualdade entre salário pago e valor produzido pelo trabalho'
+)
+criaCartao(
+    'Educação Financeira',
+    'O que o IPCA mede?',
+    'Mede a variação dos preços e serviços dos produtos que são consumidos por famílias que recebem de 1-4 salários mínimos'
+)
+criaCartao(
+    'Educação Financeira',
+    'O que é orçamento público?',
+    'É o investimento e planejamento que detalha a previsão do recursos a serem arrecadados'
+)
+criaCartao(
+    'Biologia',
+    'O que são vírus?',
+    'Parasitas acelulares, visto apenas com microscópio eletrônico'
+)
